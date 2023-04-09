07ad6811d171dc1f5ab62991360612500c21a951
Tony Le <32148504+tfle@users.noreply.github.com>
2023-04-09 19:34:35 +1000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,6 @@
-# Install Homebrew
-if test ! $(which brew); then
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-  echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $HOME/.zprofile
-  eval "$(/opt/homebrew/bin/brew shellenv)"
-  brew analytics off
-fi
+#!/bin/sh
+
+rm -rf $HOME/.zshrc
+ln -s .zshrc $HOME/.zshrc
+
+brew bundle --file ./Brewfile
