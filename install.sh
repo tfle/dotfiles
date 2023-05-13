52df99b86d0ecf5d70aec21b2bf84ee26ed51a8c
Tony Le <32148504+tfle@users.noreply.github.com>
2023-05-13 21:48:21 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-rm -rf $HOME/.zshrc
-ln -s $HOME/dotfiles/zshrc $HOME/.zshrc
-
 brew analytics off
 brew bundle --no-lock
 brew bundle --file ./Brewfile
+
+rm -rf $HOME/.zshrc
+ln -s $HOME/dotfiles/zshrc $HOME/.zshrc
